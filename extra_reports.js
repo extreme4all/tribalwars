b969655442e46de499070b1c8612c80a0cb4ac9e
extreme4all <40169115+extreme4all@users.noreply.github.com>
2019-04-12 22:47:12 +0200

    Update extra_reports.js

--- extra_reports.js
+++ extra_reports.js
@@ -1,11 +1,18 @@
+// ==UserScript==
+// @name           extra reports
+// @namespace      https://github.com/extreme4all/tribalwars/blob/master/
+// @description    Extra reports
+// @include        https://*tribalwars.nl/game.php?*screen=report*
 
+// ==/UserScript==
+var i
 function add(accumulator, a) {
   return accumulator + a;
 }
 function time(build_time_list) {
-  result_list = [];
-  building_lvl = 20;
-  time_factor = 2 / 3 * Math.pow(1.06, -building_lvl);
+  var result_list = [];
+  var building_lvl = 20;
+  var time_factor = 2 / 3 * Math.pow(1.06, -building_lvl);
   console.log(time_factor);
   for (i = 1; i < build_time_list.length; i++) {
     result_list[i] = time_factor * build_time_list[i];
@@ -31,24 +38,24 @@ function addRow(tableID, mytext) {
   var newText = document.createTextNode(mytext);
   newCell.appendChild(newText);
 }
-attacker_loss = document.getElementById("attack_info_att_units");
-def_loss = document.getElementById("attack_info_def_units");
-ta = document.getElementById("attack_info_att");
-td = document.getElementById("attack_info_def");
-a = attacker_loss.rows[2];
-d = def_loss.rows[2];
-tags = ["titel", "sp", "zw", "bl", "bo", "sc", "lc", "bb", "zc", "ram", "kata", "rid", "edel"];
-ODD_scores = ["", 1, 2, 4, 2, 2, 13, 12, 15, 8, 10, 20, 200];
-ODA_scores = ["", 4, 5, 1, 5, 1, 5, 6, 23, 4, 12, 40, 200];
-build_time = ["", 510, 750, 660, 900, 450, 900, 1350, 1800, 2400, 3600, 10800, 9000];
-real_build_time = time(build_time);
-rebuild_time_barrack = [];
-rebuild_time_stable = [];
-rebuild_time_workshop = [];
-ODD = [];
-ODA = [];
+var attacker_loss = document.getElementById("attack_info_att_units");
+var def_loss = document.getElementById("attack_info_def_units");
+var ta = document.getElementById("attack_info_att");
+var td = document.getElementById("attack_info_def");
+var a = attacker_loss.rows[2];
+var d = def_loss.rows[2];
+var tags = ["titel", "sp", "zw", "bl", "bo", "sc", "lc", "bb", "zc", "ram", "kata", "rid", "edel"];
+var ODD_scores = ["", 1, 2, 4, 2, 2, 13, 12, 15, 8, 10, 20, 200];
+var ODA_scores = ["", 4, 5, 1, 5, 1, 5, 6, 23, 4, 12, 40, 200];
+var build_time = ["", 510, 750, 660, 900, 450, 900, 1350, 1800, 2400, 3600, 10800, 9000];
+var real_build_time = time(build_time);
+var rebuild_time_barrack = [];
+var rebuild_time_stable = [];
+var rebuild_time_workshop = [];
+var ODD = [];
+var ODA = [];
 for (i = 1; i < a.querySelectorAll("td").length; i++) {
-  loss = a.cells[i].innerHTML;
+  var loss = a.cells[i].innerHTML;
   ODD[i] = loss * ODD_scores[i];
   if (i <= 4) {
     rebuild_time_barrack.push(loss * real_build_time[i]);
