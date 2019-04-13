4480a8ab7c06f8dc5cf49f692d2d2c43a235a089
extreme4all <40169115+extreme4all@users.noreply.github.com>
2019-04-14 00:09:26 +0200

    Update extra_reports.js

--- extra_reports.js
+++ extra_reports.js
@@ -46,6 +46,17 @@ function addData(node,mytext){
 function numberWithSpaces(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
+
+function getbuiltime(){
+	var opslag
+	var bt = [""]
+	$.get("interface.php?func=get_unit_info", function(data){
+	    opslag = data
+	});
+	opslag = opslag.getElementsByTagName("config")[0]
+	for (i = 0;i<opslag.childElementCount;i++){bt.push(opslag.children[i].children[0].innerHTML)}
+	return bt
+}
 var attacker_loss = document.getElementById("attack_info_att_units")
 var def_loss = document.getElementById("attack_info_def_units")
 var ta = document.getElementById("attack_info_att")
@@ -57,7 +68,7 @@ var d = def_loss.rows[2]
 var tags = ["titel","sp","zw","bl","bo","sc","lc","bb","zc","ram","kata","rid","edel"]
 var ODD_scores = ["",1,2,4,2,2,13,12,15,8,10,20,200]
 var ODA_scores = ["",4,5,1,5,1,5,6,23,4,12,40,200]
-var build_time = ["",510,750,660,900,450,900,1350,1800,2400,3600,10800,9000]
+var build_time = getbuiltime()
 var real_build_time = time(build_time);
 
 var rebuild_time_barrack = []
