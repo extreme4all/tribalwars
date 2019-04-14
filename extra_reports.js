37ec09f051ce7d1bae5191007b31f59d73588f78
extreme4all <40169115+extreme4all@users.noreply.github.com>
2019-04-14 11:35:28 +0200

    Update extra_reports.js

--- extra_reports.js
+++ extra_reports.js
@@ -49,68 +49,96 @@ function numberWithSpaces(x) {
 
 var opslag;
 var build_time = [""]
-$.get("interface.php?func=get_unit_info", function(data){
-    opslag = data
-});
-opslag = opslag.getElementsByTagName("config")[0]
-for (i = 0;i<opslag.childElementCount;i++){
-    build_time.push(opslag.children[i].children[0].innerHTML)
+function ajax(url, method, callback, params = null) {
+     var obj;
+     try {
+      obj = new XMLHttpRequest();
+     } catch(e){
+       try {
+         obj = new ActiveXObject("Msxml2.XMLHTTP");
+       } catch(e) {
+         try {
+           obj = new ActiveXObject("Microsoft.XMLHTTP");
+         } catch(e) {
+           console.log("Your browser does not support Ajax.");
+           return false;
+         }
+       }
+     }
+     obj.onreadystatechange = function() {
+      if(obj.readyState == 4) {
+         callback(obj);
+      }
+     }
+     obj.open(method, url, true);
+     obj.send(params);
+     return obj;
+ }
+ajax('interface.php?func=get_unit_info', 'get',function(obj) {
+    opslag = obj.responseXML;
+    main(opslag)
+})
+function main(opslag){
+    opslag = opslag.getElementsByTagName("config")[0]
+    for (i = 0;i<opslag.childElementCount;i++){
+        build_time.push(opslag.children[i].children[0].innerHTML)
+    }
+    var attacker_loss = document.getElementById("attack_info_att_units")
+    var def_loss = document.getElementById("attack_info_def_units")
+    var ta = document.getElementById("attack_info_att")
+    var td = document.getElementById("attack_info_def")
+
+    var a = attacker_loss.rows[2]
+    var d = def_loss.rows[2]
+
+    var tags = ["titel","sp","zw","bl","bo","sc","lc","bb","zc","ram","kata","rid","edel"]
+    var ODD_scores = ["",1,2,4,2,2,13,12,15,8,10,20,200]
+    var ODA_scores = ["",4,5,1,5,1,5,6,23,4,12,40,200]
+    var real_build_time = time(build_time);
+
+    var rebuild_time_barrack = []
+    var rebuild_time_stable = []
+    var rebuild_time_workshop = []
+    var ODD = []
+    var ODA = []
+
+    for(i = 1;i<a.querySelectorAll("td").length;i++ ){
+        var loss= a.cells[i].innerHTML
+        ODD[i] = loss * ODD_scores[i]
+        if(i<=4){
+            rebuild_time_barrack.push(loss*real_build_time[i])
+        } else if (i<=8){
+            rebuild_time_stable.push(loss*real_build_time[i])
+        } else {
+            rebuild_time_workshop.push(loss*real_build_time[i])
+        }
+    }
+
+    addData("attack_info_att",("Rebuild time workshop: " + secondsToDhms(rebuild_time_workshop.reduce(add))))
+    addData("attack_info_att",("Rebuild time stable: " + secondsToDhms(rebuild_time_stable.reduce(add))))
+    addData("attack_info_att",("Rebuild time barack: " + secondsToDhms(rebuild_time_barrack.reduce(add))))
+    console.log("ODD " + ODD.reduce(add))
+    rebuild_time_barrack = []
+    rebuild_time_stable = []
+    rebuild_time_workshop = []
+    for(i = 1;i<d.querySelectorAll("td").length-1;i++ ){
+        loss = d.cells[i].innerHTML
+        ODA[i] = loss * ODA_scores[i]
+        if(i<=4){
+            rebuild_time_barrack.push(loss*real_build_time[i])
+        } else if (i<=8){
+            rebuild_time_stable.push(loss*real_build_time[i])
+        } else {
+            rebuild_time_workshop.push(loss*real_build_time[i])
+        }
+    }
+
+    addData("attack_info_def",("Rebuild time workshop: " + secondsToDhms(rebuild_time_workshop.reduce(add))))
+    addData("attack_info_def",("Rebuild time stable: " + secondsToDhms(rebuild_time_stable.reduce(add))))
+    addData("attack_info_def",("Rebuild time barack: " + secondsToDhms(rebuild_time_barrack.reduce(add))))
+    console.log("ODA " + ODA.reduce(add))
+
+    addData("attack_info_def",("ODD " + numberWithSpaces(ODD.reduce(add))))
+    addData("attack_info_att",("ODA " + numberWithSpaces(ODA.reduce(add))))
+
 }
-
-var attacker_loss = document.getElementById("attack_info_att_units")
-var def_loss = document.getElementById("attack_info_def_units")
-var ta = document.getElementById("attack_info_att")
-var td = document.getElementById("attack_info_def")
-
-var a = attacker_loss.rows[2]
-var d = def_loss.rows[2]
-
-var tags = ["titel","sp","zw","bl","bo","sc","lc","bb","zc","ram","kata","rid","edel"]
-var ODD_scores = ["",1,2,4,2,2,13,12,15,8,10,20,200]
-var ODA_scores = ["",4,5,1,5,1,5,6,23,4,12,40,200]
-var real_build_time = time(build_time);
-
-var rebuild_time_barrack = []
-var rebuild_time_stable = []
-var rebuild_time_workshop = []
-var ODD = []
-var ODA = []
-
-for(i = 1;i<a.querySelectorAll("td").length;i++ ){
-	var loss= a.cells[i].innerHTML
-	ODD[i] = loss * ODD_scores[i]
-	if(i<=4){
-		rebuild_time_barrack.push(loss*real_build_time[i])
-	} else if (i<=8){
-		rebuild_time_stable.push(loss*real_build_time[i])
-	} else {
-		rebuild_time_workshop.push(loss*real_build_time[i])
-	}
-}
-
-addData("attack_info_att",("Rebuild time workshop: " + secondsToDhms(rebuild_time_workshop.reduce(add))))
-addData("attack_info_att",("Rebuild time stable: " + secondsToDhms(rebuild_time_stable.reduce(add))))
-addData("attack_info_att",("Rebuild time barack: " + secondsToDhms(rebuild_time_barrack.reduce(add))))
-console.log("ODD " + ODD.reduce(add))
-rebuild_time_barrack = []
-rebuild_time_stable = []
-rebuild_time_workshop = []
-for(i = 1;i<d.querySelectorAll("td").length-1;i++ ){
-	loss = d.cells[i].innerHTML
-	ODA[i] = loss * ODA_scores[i]
-	if(i<=4){
-		rebuild_time_barrack.push(loss*real_build_time[i])
-	} else if (i<=8){
-		rebuild_time_stable.push(loss*real_build_time[i])
-	} else {
-		rebuild_time_workshop.push(loss*real_build_time[i])
-	}
-}
-
-addData("attack_info_def",("Rebuild time workshop: " + secondsToDhms(rebuild_time_workshop.reduce(add))))
-addData("attack_info_def",("Rebuild time stable: " + secondsToDhms(rebuild_time_stable.reduce(add))))
-addData("attack_info_def",("Rebuild time barack: " + secondsToDhms(rebuild_time_barrack.reduce(add))))
-console.log("ODA " + ODA.reduce(add))
-
-addData("attack_info_def",("ODD " + numberWithSpaces(ODD.reduce(add))))
-addData("attack_info_att",("ODA " + numberWithSpaces(ODA.reduce(add))))
